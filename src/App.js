9c96bd22749ea8673160fbf03b072cf551182ba6
Jay Lee <leeja744@student.otago.ac.nz>
2023-09-15 14:26:50 +1200

    draft about me page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
+import portrait from './images/portrait.JPG'
 
 function App() {
   const targetRef = useRef(null);
+  // const [showNavigationBar, setShowNavigationBar] = useState(false);
 
   const scrollToTarget = () => {
     if (targetRef.current) {
@@ -11,23 +13,22 @@ function App() {
 
   return (
     <div>
-      <div className="w-screen h-screen flex flex-col items-center justify-center">
-        <h1 className="text-8xl font-bold">Jay Lee</h1>
-        <h2 className="text-lg font-semibold font-serif mt-2">Software Developer</h2>
-        <span onClick={scrollToTarget} className="text-2xl animate-bounce hover:cursor-pointer">&#8650;</span>
+      <div className="w-screen h-screen flex flex-col items-center justify-center bg-black">
+        <h1 className="text-8xl font-bold text-white">Jay Lee</h1>
+        <h2 className="text-lg font-semibold font-serif mt-2 text-white">Software Developer</h2>
+        <span onClick={scrollToTarget} className="text-2xl animate-bounce hover:cursor-pointer text-white">&#8650;</span>
       </div>
-      <div className="w-screen h-screen flex flex-row items-center justify-center bg-slate-400">
-        <div className="basis-1/3 h-full bg-yellow-200"></div>
-        <div ref={targetRef} className="basis-2/3 h-full">
-          <h1 className="text-8xl font-bold">About Me</h1>
-          <h2 className="text-lg font-semibold font-serif mt-2">Software Developer</h2>
-          <span className="text-2xl animate-bounce hover:font-bold hover:cursor-pointer">&#8650;</span>
+      <div ref={targetRef} className="w-screen h-screen flex flex-row bg-black">
+        <div className="basis-1/2 h-full flex flex-col justify-center items-end" >
+          <h1 className="text-6xl font-bold m-11 justify-end  text-white">About Me</h1>
+          <p className='w-3/4 text-end text-white'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        </div>
+        <div className="basis-1/2 h-full flex justify-center items-center">
+          <img className= "w-4/6 h-4/6 object-contain" src={portrait} alt="Portrait of Jay Lee"></img>
         </div>
       </div>
       <div className="w-screen h-screen flex flex-col items-center justify-center bg-black">
         <h1 className="text-8xl font-bold text-white">My Work</h1>
-        <h2 className="text-lg font-semibold font-serif mt-2 text-white">Software Developer</h2>
-        <span className="text-2xl animate-bounce hover:font-bold hover:cursor-pointer text-white">&#8650;</span>
       </div>
     </div>
   );
---
+++ src/components/navigation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NavigationBar() {
+  return (
+    <div className="sticky top-0 w-full h-16 bg-orange-100 text-white flex flex-row">
+        <div>Hello</div>
+    </div>
+  );
+}
+
+export default NavigationBar;
