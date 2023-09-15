1252c83e9da43a40435987f87bd0a51f14cafeb1
Jay Lee <leeja744@student.otago.ac.nz>
2023-09-15 14:42:16 +1200

    added headshot

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useRef, useEffect, useState } from 'react';
 import portrait from './images/portrait.JPG'
+import headshot from './images/headshot.JPG'
 
 function App() {
   const targetRef = useRef(null);
@@ -14,7 +15,10 @@ function App() {
   return (
     <div>
       <div className="w-screen h-screen flex flex-col items-center justify-center bg-black">
-        <h1 className="text-8xl font-bold text-white">Jay Lee</h1>
+        <div class="w-44 h-44 rounded-full overflow-hidden">
+          <img src={headshot} alt="Image Description" className="w-full h-full object-cover"></img>
+        </div>
+        <h1 className="text-8xl font-bold text-white justify-self-center">Jay Lee</h1>
         <h2 className="text-lg font-semibold font-serif mt-2 text-white">Software Developer</h2>
         <span onClick={scrollToTarget} className="text-2xl animate-bounce hover:cursor-pointer text-white">&#8650;</span>
       </div>
@@ -24,11 +28,12 @@ function App() {
           <p className='w-3/4 text-end text-white'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         </div>
         <div className="basis-1/2 h-full flex justify-center items-center">
-          <img className= "w-4/6 h-4/6 object-contain" src={portrait} alt="Portrait of Jay Lee"></img>
+          <img src={portrait} alt="Portrait of Jay Lee" className="w-4/6 h-4/6 object-contain" ></img>
         </div>
       </div>
-      <div className="w-screen h-screen flex flex-col items-center justify-center bg-black">
-        <h1 className="text-8xl font-bold text-white">My Work</h1>
+      <div className="w-screen flex flex-col items-center justify-center bg-emerald-800">
+        <h1 className="text-6xl font-bold text-white">My Work</h1>
+        <div className='h-96'>Content goes here</div>
       </div>
     </div>
   );
